941eafdbcaf3d23cf9b287547afe5e570cc098df
kcw0x466 <kcw774707@gmail.com>
2024-11-15 21:43:05 +0900

    UI 구성 완료

--- Client/SoundTube/app/src/main/java/com/cookandroid/soundtube/MainActivity.java
+++ Client/SoundTube/app/src/main/java/com/cookandroid/soundtube/MainActivity.java
@@ -2,17 +2,41 @@ package com.cookandroid.soundtube;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.appbar_action, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_settings1:
+//                Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_LONG).show();
+//                return true;
+//
+//        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
+
