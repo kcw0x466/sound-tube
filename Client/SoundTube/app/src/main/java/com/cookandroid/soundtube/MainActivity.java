539feba5e201a0b9f3759df674f7321003f2a660
Chanwook Kim <kcw774707@gmail.com>
2024-11-18 02:00:12 +0900

    화면 전환 구현

--- Client/SoundTube/app/src/main/java/com/cookandroid/soundtube/MainActivity.java
+++ Client/SoundTube/app/src/main/java/com/cookandroid/soundtube/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.soundtube;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
@@ -7,16 +8,54 @@ import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_Navi_Bar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("노래");
+        getSupportActionBar().setLogo(R.drawable.round_audiotrack);
+
+        // 처음 화면
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new MusicList()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.menu_music){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MusicList()).commit();
+                    getSupportActionBar().setTitle("노래");
+                    getSupportActionBar().setLogo(R.drawable.round_audiotrack);
+                }
+                else if (item.getItemId() == R.id.menu_playlist) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new PlayList()).commit();
+                    getSupportActionBar().setTitle("재생목록");
+                    getSupportActionBar().setLogo(R.drawable.round_playlist_play);
+                }
+                else if (item.getItemId() == R.id.menu_quick_play) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new QuickPlay()).commit();
+                    getSupportActionBar().setTitle("퀵 플레이");
+                    getSupportActionBar().setLogo(R.drawable.round_music_video_24);
+                }
+                return true;
+            }
+        });
+
+
 
     }
 
